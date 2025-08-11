d2991ef54e6d2df90cdd050c171dbedeaea566aa
Aakansha Priya <aakanshapriya2000@gmail.com>
2025-08-12 00:32:56 +0530

    latest commit

--- instrumentation.ts
+++ instrumentation.ts
@@ -3,11 +3,11 @@ import { registerOTel } from "@vercel/otel";
 export function register() {
   registerOTel({
     // Add your service name here instead of the placeholder
-    serviceName: "aakansha-vercel-tracing",
+    serviceName: "dash0-vercel-demo",
     instrumentationConfig: {
       fetch: {
         // By default the context is only propagated for the deployment URLs, all other URLs should be enabled explicitly in the list by string prefix or regex.
-        propagateContextUrls: [],
+        propagateContextUrls: ['https://dash0-vercel-demo.vercel.app/'],
       },
     },
   });
